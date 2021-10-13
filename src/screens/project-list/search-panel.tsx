5558d3ce1459efdec7e642f1bc3585a973bc7475
toyouh <xjh1246909068@163.com>
2021-10-13 15:51:25 +0800

    5.chore: 添加ts类型定义

---
+++ src/screens/project-list/search-panel.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  title: string;
+  organization: string;
+}
+
+interface SearchPanelProps {
+  users: User[];
+  param: {
+    name: string;
+    personId: string;
+  };
+  setParam: (param: SearchPanelProps["param"]) => void; //void代表什么也不返回
+}
+
+export const SearchPanel = ({ param, setParam, users }: SearchPanelProps) => {
+  return (
+    <form>
+      <div>
+        {/* setParam(Object.assign({},param,{name:evt.target.value})) */}
+        <input
+          type="text"
+          value={param.name}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              name: evt.target.value,
+            })
+          }
+        />
+        <select
+          value={param.personId}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              personId: evt.target.value,
+            })
+          }
+        >
+          <option value="" key="all key">
+            负责人
+          </option>
+          {users.map((user) => (
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </form>
+  );
+};
